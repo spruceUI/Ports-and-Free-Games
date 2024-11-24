8a8d688f3ce0aeb149a610bb21f7fc46a969efc3
cobaltgit <cobaltmail4204@gmail.com>
2024-11-24 15:00:08 +0000

    Use ra32.miyoo

--- Ports/Cannonball/Roms/PORTS/Cannonball.sh
+++ Ports/Cannonball/Roms/PORTS/Cannonball.sh
@@ -6,4 +6,4 @@ CANNONBALL_DIR="$ROM_DIR/cannonball"
 CORE_DIR="$RA_DIR/.retroarch/cores"
 
 cd "$RA_DIR"
-./retroarch -v -L "$CORE_DIR/cannonball_libretro.so" "$CANNONBALL_DIR/CannonBall.game"
+./ra32.miyoo -v -L "$CORE_DIR/cannonball_libretro.so" "$CANNONBALL_DIR/CannonBall.game"
