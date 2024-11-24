cb175e1bce8e30d69c1474432756a35fba649b8a
cobaltgit <cobaltmail4204@gmail.com>
2024-11-24 20:11:27 +0000

    Add openjazz port Contains jazz v1.1 and holiday hare shareware data files

---
+++ Ports/Jazz Jackrabbit/Roms/PORTS/Jazz Jackrabbit.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ROM_DIR="$(dirname "$0")"
+OPENJAZZ_DIR="$PWD/openjazz"
+LD_LIBRARY_PATH="$OPENJAZZ_DIR/lib:/mnt/SDCARD/miyoo/lib:$LD_LIBRARY_PATH"
+
+cd "$OPENJAZZ_DIR"
+
+./OpenJazz jazz11 jazzxmas
