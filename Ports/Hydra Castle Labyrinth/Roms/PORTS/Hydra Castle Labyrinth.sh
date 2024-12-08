f7c1b16ca3164b963e8ecd2d34df93fe2db2cc8e
cobaltgit <cobaltmail4204@gmail.com>
2024-12-08 12:05:00 +0000

    Add analog stick control to hcl

--- Ports/Hydra Castle Labyrinth/Roms/PORTS/Hydra Castle Labyrinth.sh
+++ Ports/Hydra Castle Labyrinth/Roms/PORTS/Hydra Castle Labyrinth.sh
@@ -5,4 +5,10 @@ HCL_DIR="$ROM_DIR/hydracastlelabyrinth"
 export LD_LIBRARY_PATH="/mnt/SDCARD/miyoo/lib:$LD_LIBRARY_PATH"
 cd "$HCL_DIR"
 
+# set stick to d-pad mode
+killall -q -USR2 joystickinput
+
 ./hcl
+
+# set stick to analog mode
+killall -q -USR1 joystickinput
